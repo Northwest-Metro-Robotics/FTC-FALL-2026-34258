dc7546862d6fd9dad3fb843b9e629a60e7f3b910
ariaclark-eng <aria.clark@nwmrobotics.org>
2026-09-03 18:23:55 -0500

    return to basic teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/BasicTeleOp.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "Basic TeleOp", group = "Driver Control")
+public class BasicTeleOp extends LinearOpMode {
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    @Override
+    public void runOpMode() {
+        frontLeft = hardwareMap.get(DcMotor.class, "DLF");
+        frontRight = hardwareMap.get(DcMotor.class, "DRF");
+        backLeft = hardwareMap.get(DcMotor.class, "DLB");
+        backRight = hardwareMap.get(DcMotor.class, "DRB");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        telemetry.addLine("Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double drive = -gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+
+            double leftPower = clip(drive + turn);
+            double rightPower = clip(drive - turn);
+
+            frontLeft.setPower(leftPower);
+            backLeft.setPower(leftPower);
+            frontRight.setPower(rightPower);
+            backRight.setPower(rightPower);
+
+            telemetry.addData("Left Power", "%.2f", leftPower);
+            telemetry.addData("Right Power", "%.2f", rightPower);
+            telemetry.update();
+        }
+    }
+
+    private double clip(double value) {
+        return Math.max(-1.0, Math.min(1.0, value));
+    }
+}
